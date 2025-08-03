18bd0db77472f87433c2bc2b54515eccdeb4316d
lucasftorres <lucaas.torresf192@gmail.com>
2025-08-02 23:40:13 -0300

    extract

---
+++ app/main.py
@@ -0,0 +1,4 @@
+from pipeline.extract import extract_from_excel
+
+listas_de_data_frame = extract_from_excel('C:/Users/Lucas/Documents/jornada_projects/workshop_estrutura_do_zero/data/input')
+print(listas_de_data_frame)
