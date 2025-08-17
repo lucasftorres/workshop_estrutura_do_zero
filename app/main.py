f48f6f86491fea2dffc387edb81fdbc77143acb2
lucasftorres <lucaas.torresf192@gmail.com>
2025-08-16 23:57:47 -0300

    update

--- app/main.py
+++ app/main.py
@@ -6,5 +6,4 @@ if __name__ == '__main__':
     data_frame_list = extract_from_excel('data/input')
     data_frame = concat_data_frames(data_frame_list)
     load_excel(data_frame, 'data/output', 'output')
-# listas_de_data_frame = extract_from_excel('C:/Users/Lucas/Documents/jornada_projects/workshop_estrutura_do_zero/data/input')
-# print(listas_de_data_frame)
+
